28e891ca3837d33f5a30527b73d1e9e72843687b
chloe alcorn <ca.alcorn@outlook.com>
2018-10-08 16:05:17 +0100

    calculator homework completed

---
+++ Calculator/src/test/java/calculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class calculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(4, calculator.add(2,2));
+    }
+    @Test
+    public void canSubract(){
+        assertEquals(1, calculator.subtract(3,2));
+    }
+    @Test
+    public void canMultiply(){
+        assertEquals(16, calculator.multiply(4,4));
+    }
+    @Test
+    public void canDivide(){
+        assertEquals(3, calculator.divide(21,7));
+    }
+}
